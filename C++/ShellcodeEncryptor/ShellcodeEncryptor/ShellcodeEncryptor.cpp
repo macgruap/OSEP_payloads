6bd9448a0814046fdfff8e62b938b8601cae0931
Luis Vázquez <luisvqz93@gmail.com>
2023-10-06 12:43:03 +0200

    Removed regex from encryptor. Big payloads dumped to file.

--- C++/ShellcodeEncryptor/ShellcodeEncryptor/ShellcodeEncryptor.cpp
+++ C++/ShellcodeEncryptor/ShellcodeEncryptor/ShellcodeEncryptor.cpp
@@ -26,16 +26,31 @@ int main(int argc, char* argv[])
 		stringstream ss;
 		ss << t.rdbuf();
 		string shellcode = ss.str();
+		printf("Processing payload...\n");
 		shellcode = shellcode.substr(shellcode.find_last_of("=") + 1, shellcode.size());
-		regex reg("\t|\"|\n|;| |\\\\|x");
-		shellcode = regex_replace(shellcode, reg, "");
+		shellcode.erase(remove(shellcode.begin(), shellcode.end(), '\t'), shellcode.end());
+		shellcode.erase(remove(shellcode.begin(), shellcode.end(), '\"'), shellcode.end());
+		shellcode.erase(remove(shellcode.begin(), shellcode.end(), '\n'), shellcode.end());
+		shellcode.erase(remove(shellcode.begin(), shellcode.end(), ';'), shellcode.end());
+		shellcode.erase(remove(shellcode.begin(), shellcode.end(), ' '), shellcode.end());
+		shellcode.erase(remove(shellcode.begin(), shellcode.end(), '\\'), shellcode.end());
+		shellcode.erase(remove(shellcode.begin(), shellcode.end(), 'x'), shellcode.end());
 		int len = shellcode.length() / 2;
 		unsigned char* buf = new unsigned char[len];
+		cout << "0%";
 		const char* cstr = shellcode.c_str();
+		int ref = 0;
 		for (int i = 0; i < len; i++) {
 			sscanf_s(cstr, "%2hhx", &buf[i]);
 			cstr += 2 * sizeof(char);
-		}
+			float percent = ((float)i / (float)len) * 100;
+			if ((int)percent >= ref+10) {
+ 				printf("\33[2K\r");
+				cout << (int) percent << "%";
+				ref = (int)percent;
+			}
+ 		}
+		printf("\33[2K\r");
 		int originalLen = len;
 		int paddedMessageLen = originalLen;
 		if ((paddedMessageLen % 16) != 0) {
@@ -50,30 +65,58 @@ int main(int argc, char* argv[])
 				paddedMessage[i] = buf[i];
 			}
 		}
+
 		unsigned char* encryptedMessage = new unsigned char[paddedMessageLen];
 		unsigned char expandedKey[176];
 		KeyExpansion(key, expandedKey);
 		for (int i = 0; i < paddedMessageLen; i += 16) {
 			AESEncrypt(paddedMessage + i, expandedKey, encryptedMessage + i);
 		}
-		printf("Key: %s (", key);
+		printf("Done!\n\nKey: %s (", key);
 		for (int i = 0; i < sizeof key; i++) {
 			cout << hex << (int)key[i];
 		}
 		cout << ")\n\n";
-		printf("unsigned char buf[] = \n\t\"");
-		for (int i = 0; i < paddedMessageLen; i++) {
-			if ((i + 1) % 14 == 0) {
-				cout << "\\x" << std::setfill('0') << std::setw(2) << hex << (int)encryptedMessage[i];
-				cout << "\"\n\t\"";
+
+		if (paddedMessageLen < 4096) {
+			printf("unsigned char buf[] = \n\t\"");
+			for (int i = 0; i < paddedMessageLen; i++) {
+				if ((i + 1) % 14 == 0) {
+					cout << "\\x" << std::setfill('0') << std::setw(2) << hex << (int)encryptedMessage[i];
+					cout << "\"\n\t\"";
+				}
+				else {
+					cout << "\\x" << std::setfill('0') << std::setw(2) << hex << (int)encryptedMessage[i];
+				}
 			}
-			else {
-				cout << "\\x" << std::setfill('0') << std::setw(2) << hex << (int)encryptedMessage[i];
-			}
+			cout << "\";";
 		}
-		cout << "\";";
-		delete[] paddedMessage;
-		delete[] encryptedMessage;
+		else {
+			ofstream outFile;
+			ostringstream content;
+
+			string origFile = argv[1];
+			string path = origFile.substr(0, origFile.find_last_of("\\"))+"\\encPayload.txt";
+
+			printf("Encrypted payload too big to be displayed! Dumping it to %s...\n", path.c_str());
+			
+			content << "unsigned char buf[] = \n\t\"";
+			bool AAA = false;
+			for (int i = 0; i < paddedMessageLen; i++) {
+				if ((i + 1) % 14 == 0) {
+					content << "\\x" << std::setfill('0') << std::setw(2) << hex << (int)encryptedMessage[i];
+					content << "\"\n\t\"";
+				}
+				else {
+					content << "\\x" << std::setfill('0') << std::setw(2) << hex << (int)encryptedMessage[i];
+				}
+			}
+			content << "\";";
+			outFile.open(path);
+			outFile << content.str();
+			outFile.close();
+			printf("Done!");
+		}
 		return 0;
 	}
 	return -1;
